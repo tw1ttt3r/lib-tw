8774f3fb7f6fd8d896f8c96e7c27ad0e86f518aa
tw1ttt3r <tw1ttt3r@gmail.com>
2023-05-09 10:20:27 -0500

    feat: added rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import { terser } from 'rollup-plugin-terser';
 import dts from "rollup-plugin-dts";
+import tailwind from "rollup-plugin-tailwindcss";
+import postcss from "rollup-plugin-postcss";
 import packageJson from "./package.json" assert { type: "json" };
 
 export default [
@@ -25,6 +27,13 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss({ extract: true, process: processScss}),
+      tailwind({
+        input: './styles/output.css', // required
+        // Tailor the emitted stylesheet to the bundle by removing any unused CSS
+        // (highly recommended when packaging for distribution).
+        purge: false,
+      }),
       terser()
     ],
   },
