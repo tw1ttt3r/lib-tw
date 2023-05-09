eeedaba2deef0c35be75a48eb7b75dd8e1d16b5b
tw1ttt3r <tw1ttt3r@gmail.com>
2023-05-08 20:19:38 -0500

    feat: restructured components

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,1 @@
-import React from 'react';
-
-export const Hello: React.FC<{ name: String }> = ({name}) => {
-    if (!name) {
-      name = 'Mundo';
-    }
-    return (<h1 className='text-red-300 font-bold'>¡¡Hola {name}!!</h1>)
-}
+export * from '@/lib/message';
